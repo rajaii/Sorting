c528a6e7bfe5aa2c0222e9355308a56273b8fef3
Ali Rajaii <rajaii_a@yahoo.com>
2020-03-17 11:43:42 -0400

    MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,47 +1,34 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
-
+    for i in range(len(arr) - 1):
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp         
+    print(arr)
     return arr
+    
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    # swapped = True
-    # while swapped == True:
-        for i in range(len(arr) - 1):
-            comparison_done = False
-            while comparison_done == False:
-                for j in range(i+1, len(arr)):
-                    print(f'i: {arr[i]}, j: {arr[j]}')
-                    if arr[i] > arr[j]:
-                        print()
-                        temp = arr[i]
-                        print(f'temp: {temp}')
-                        arr[i] = arr[j]
-                        arr[j] = temp
-                        print(f'{arr[i]}, {arr[j]}')
-                        comparison_done = True
-                    else:
-                        comparison_done = True
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(0, len(arr) - 1):
+            
+            print(f'{i} = {arr[i]}; {i+1} = {arr[i+1]}')
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped = True
+    return arr
+        
+  
+        
 
-        print(arr)
-        return arr
-
-arr = [3,2,4,7,9,6]
+arr = [1, 5, 8, 4, 2]
 bubble_sort(arr)
 
 # STRETCH: implement the Count Sort function below
